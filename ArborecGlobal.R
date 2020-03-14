31ed566f85a1d370ed8d21dbc004610022d8e119
Danilo Farias <dansoaresfarias@gmail.com>
2020-03-14 15:32:00 -0300

    Enviando as correções de corte de base e inclusão do código de adabag.

--- ArborecGlobal.R
+++ ArborecGlobal.R
@@ -7,39 +7,54 @@ require(rpart.plot)
 require(party)
 require(partykit)
 require(tree)
+library(adabag)
 
+#Carga da base
 ArboRec <- read_delim("ArboRec.csv", ";", escape_double = FALSE, trim_ws = TRUE)
 
 # Transformando o tp_classificacao_final em: Descartado, Inconclusivo, Dengue
 ArboRec$tp_classificacao_final = ifelse(ArboRec$tp_classificacao_final == 5, 'Descartado', ifelse(ArboRec$tp_classificacao_final == 8, 'Inconclusivo', 'Dengue'))
+table(ArboRec$tp_classificacao_final)
 
 #Modificando os tipos de dados para Factor
-
 ArboRec$tp_classificacao_final <- as.factor(ArboRec$tp_classificacao_final)
-ArboRec$notificacao_ano <- as.factor(ArboRec$notificacao_ano)
-ArboRec$ds_semana_notificacao <- as.factor(ArboRec$ds_semana_notificacao)
 ArboRec$co_distrito_residencia <- as.factor(ArboRec$co_distrito_residencia)
-ArboRec$co_logradouro_residencia <- as.factor(ArboRec$co_logradouro_residencia)
 ArboRec$tp_sexo <- as.factor(ArboRec$tp_sexo)
+ArboRec$tp_gestante <- as.factor(ArboRec$tp_gestante)
 ArboRec$tp_raca_cor <- as.factor(ArboRec$tp_raca_cor)
 ArboRec$tp_escolaridade <- as.factor(ArboRec$tp_escolaridade)
+ArboRec$co_distrito_residencia <- as.factor(ArboRec$co_distrito_residencia)
+ArboRec$no_bairro_residencia <- as.factor(ArboRec$no_bairro_residencia)
 ArboRec$tp_zona_residencia <- as.factor(ArboRec$tp_zona_residencia)
 ArboRec$tp_result_exame <- as.factor(ArboRec$tp_result_exame)
 
-plot(ArboRec$tp_classificacao_final)
+#Recortando a base para apenas as colunas usadas para o modelo
+ArboRecFinal <- ArboRec[, c(14:17, 21, 23, 26, 57, 68, 74)]
 
-particaoArbo = createDataPartition(1:nrow(ArboRec), p=.7) 
-treinoArbo = ArboRec[particaoArbo$Resample1,] 
-testeArbo = ArboRec[- particaoArbo$Resample1,] 
+#table(ArboRecFinal$tp_result_exame)
+plot(ArboRecFinal$tp_classificacao_final)
+
+#Criação da base de treinamento e de teste
+particaoArbo = createDataPartition(1:nrow(ArboRecFinal), p=.7) 
+treinoArbo = ArboRecFinal[particaoArbo$Resample1,] 
+testeArbo = ArboRecFinal[- particaoArbo$Resample1,] 
 
 ##Análise de NaiveBayes
 
-arboNaiveBayes = naiveBayes(tp_classificacao_final ~  notificacao_ano + ds_semana_notificacao + no_bairro_residencia + co_distrito_residencia + tp_sexo + tp_raca_cor + tp_escolaridade + tp_zona_residencia + tp_result_exame, treinoArbo)
-
+arboNaiveBayes = naiveBayes(tp_classificacao_final ~., treinoArbo)
 arboNaiveBayes
 
 previsaoArbo = predict(arboNaiveBayes, testeArbo) 
 confusionMatrix(previsaoArbo, testeArbo$tp_classificacao_final)
+
+### Adaboost ####
+
+adabagArbo <- boosting(tp_classificacao_final ~ ., data=treinoArbo, boos=TRUE, mfinal=20, coeflearn='Breiman')
+plot(errorevol(adabagArbo,treinoArbo))
+
+previsaoArbo = predict(adabagArbo, testeArbo) # criar predição
+confusionMatrix(as.factor(previsaoArbo$class), testeArbo$tp_classificacao_final) # matriz de confusão
+
 
 ##Criação de Arvore de Decisão
 
@@ -78,4 +93,3 @@ previsaoPartArbo = as.data.frame(previsaoPartArbo)
 confusionMatrix(previsaoPartArbo$previsaoPartArbo, testeArbo$tp_classificacao_final)
 
 save(arboRpart, file="arboRpart.Rdata")
-
